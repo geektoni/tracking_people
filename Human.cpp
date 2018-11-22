d532ed04e6420c392695da35cb5bbfc6d3c56172
Giovanni De Toni <giovanni.det@gmail.com>
2018-11-22 12:30:11 +0100

    Implemented all the basic definitions.

--- Human.cpp
+++ Human.cpp
@@ -12,3 +12,21 @@ Human::Human(int id)
 	// Set a random color
 	color = Scalar (rand()%255, rand()%255, rand()%255);
 }
+
+bool Human::is_the_same(const Point2f position)
+{
+	// Compute distance between points
+	float distance = sqrt(pow(this->current_position.x-position.x, 2)+pow(this->current_position.y-position.y,2));
+
+	return !(distance > this->position_error);
+}
+
+void Human::add_to_trace(const Point2f point)
+{
+	this->trace.push_back(point);
+}
+
+void Human::update_position(const Point2f position)
+{
+	this->current_position = position;
+}
